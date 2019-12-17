5d41e1e9a8e5784ff201458f97ba414fbed1179d
Jonas Knobloch <jonas.knobloch@t-online.de>
2019-12-17 19:53:50 +0100

    Test statement helpers

--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module github.com/neverhaveiever-io/api
 go 1.12
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.3.3
 	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a // indirect
 	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-ozzo/ozzo-validation v3.5.0+incompatible
 	github.com/google/uuid v1.1.1
 	github.com/jinzhu/gorm v1.9.8
+	github.com/lib/pq v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.8 // indirect
-	github.com/moogar0880/problems v0.1.1
 	github.com/spf13/viper v1.4.0
 )
---
+++ internal/statement/helpers_test.go
@@ -0,0 +1,131 @@
+package statement
+
+import (
+	"database/sql"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
+	"github.com/jinzhu/gorm"
+	"github.com/neverhaveiever-io/api/internal/category"
+	"github.com/neverhaveiever-io/api/internal/database"
+	"os"
+	"regexp"
+	"testing"
+	"time"
+)
+
+var row = []string{
+	"id",
+	"statement",
+	"category",
+	"created_at",
+	"updated_at",
+	"deleted_at",
+}
+
+var expected = &Statement{
+	ID:        uuid.New(),
+	Statement: "Never have I ever fucked a coconut.",
+	Category:  category.Offensive,
+	CreatedAt: time.Now(),
+	UpdatedAt: time.Now(),
+	DeletedAt: nil,
+}
+
+var mock sqlmock.Sqlmock
+
+func init() {
+	var db *sql.DB
+	var err error
+
+	db, mock, err = sqlmock.New()
+
+	if err != nil {
+		panic(err)
+	}
+
+	connection, err := gorm.Open("postgres", db)
+
+	if err != nil {
+		panic(err)
+	}
+
+	database.C = connection
+
+	if _, ok := os.LookupEnv("LOG_MODE"); ok {
+		database.C.LogMode(ok)
+	}
+}
+
+func TestGetByID(t *testing.T) {
+	query := `SELECT * FROM "statements" WHERE "statements"."deleted_at" IS NULL AND (("statements"."id" = $1))`
+
+	mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(expected.ID).WillReturnRows(
+		sqlmock.NewRows(row).AddRow(
+			expected.ID.String(),
+			expected.Statement,
+			expected.Category.String(),
+			expected.CreatedAt,
+			expected.UpdatedAt,
+			expected.DeletedAt,
+		),
+	)
+
+	statement, err := GetByID(expected.ID)
+
+	if err != nil {
+		t.Fatalf("Unexpected error. %+v", err)
+	}
+
+	if statement != *expected {
+		t.Fatalf("Unexpected struct contents. %+v", statement)
+	}
+}
+
+func TestGetByIDReturnsErrorIfStatementNotFound(t *testing.T) {
+	query := `SELECT * FROM "statements" WHERE "statements"."deleted_at" IS NULL AND (("statements"."id" = $1))`
+
+	mock.ExpectQuery(regexp.QuoteMeta(query)).WillReturnRows(sqlmock.NewRows(row))
+
+	_, err := GetByID(uuid.New())
+
+	if !gorm.IsRecordNotFoundError(err) {
+		t.Fatalf("Unexpected error. %+v", err)
+	}
+}
+
+func TestGetRandomByCategory(t *testing.T) {
+	query := `SELECT * FROM "statements" WHERE "statements"."deleted_at" IS NULL AND (("statements"."category" = $1)) ORDER BY random(),"statements"."id" ASC LIMIT 1`
+
+	mock.ExpectQuery(regexp.QuoteMeta(query)).WillReturnRows(
+		sqlmock.NewRows(row).AddRow(
+			expected.ID.String(),
+			expected.Statement,
+			expected.Category.String(),
+			expected.CreatedAt,
+			expected.UpdatedAt,
+			expected.DeletedAt,
+		),
+	)
+
+	statement, err := GetRandomByCategory(category.Offensive)
+
+	if err != nil {
+		t.Fatalf("Unexpected error. %+v", err)
+	}
+
+	if statement != *expected {
+		t.Fatalf("Unexpected struct contents. %+v", statement)
+	}
+}
+
+func TestGetRandomByCategoryReturnsErrorIfStatementNotFound(t *testing.T) {
+	query := `SELECT * FROM "statements" WHERE "statements"."deleted_at" IS NULL AND (("statements"."category" = $1)) ORDER BY random(),"statements"."id" ASC LIMIT 1`
+
+	mock.ExpectQuery(regexp.QuoteMeta(query)).WillReturnRows(sqlmock.NewRows(row))
+
+	_, err := GetRandomByCategory(category.Offensive)
+
+	if !gorm.IsRecordNotFoundError(err) {
+		t.Fatalf("Unexpected error. %+v", err)
+	}
+}
