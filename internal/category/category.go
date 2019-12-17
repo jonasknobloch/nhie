1148ea3339f7afac318ac661b9c9860a9328f239
Jonas Knobloch <jonas.knobloch@t-online.de>
2019-12-17 19:48:35 +0100

    Support string type for source in category scan
    * Varchar column leads to source being a string

--- internal/category/category.go
+++ internal/category/category.go
@@ -2,6 +2,7 @@ package category
 
 import (
 	"database/sql/driver"
+	"fmt"
 )
 
 type Category string
@@ -12,11 +13,23 @@ const (
 	Offensive Category = "offensive"
 )
 
-func (c *Category) Scan(value interface{}) error {
-	*c = Category(value.([]byte))
-	return nil
+func (c *Category) Scan(src interface{}) error {
+	switch src := src.(type) {
+	case []byte: // text column
+		*c = Category(src)
+		return nil
+	case string: // varchar column
+		*c = Category(src)
+		return nil
+	default:
+		return fmt.Errorf("scan: unable to scan type %T into Category", src)
+	}
 }
 
 func (c Category) Value() (driver.Value, error) {
-	return string(c), nil
+	return c.String(), nil
+}
+
+func (c Category) String() string {
+	return string(c)
 }
