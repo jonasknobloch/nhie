0a669a76cf1d8a54ddbbbb9ba696b14b230f5f15
Jonas Knobloch <jonas.knobloch@t-online.de>
2020-03-15 17:21:10 +0100

    Add prometheus monitoring #4

--- internal/app/router/router.go
+++ internal/app/router/router.go
@@ -4,19 +4,23 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/neverhaveiever-io/api/internal/app/middleware/error"
+	"github.com/neverhaveiever-io/api/internal/app/middleware/prometheus"
 	v1 "github.com/neverhaveiever-io/api/internal/app/router/v1"
 )
 
-func Init(auth gin.HandlerFunc) {
+func Init(accounts gin.Accounts) {
 
 	router := gin.Default()
 
 	router.Use(cors.Default())
 	router.Use(error.Error())
 
+	// initialize prometheus metrics
+	prometheus.UseWithAuth(router, accounts)
+
 	router.GET("/v1/statements/:id", v1.GetStatement)
 
-	authorized := router.Group("/v1", auth)
+	authorized := router.Group("/v1", gin.BasicAuth(accounts))
 	{
 		authorized.POST("/statements", v1.AddStatement)
 		authorized.PUT("/statements/:id", v1.EditStatement)
