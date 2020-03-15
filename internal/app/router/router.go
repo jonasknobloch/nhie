d8a9279dd0d02607a64eb58aecf9929c7c561a83
Jonas Knobloch <jonas.knobloch@t-online.de>
2020-03-15 18:39:12 +0100

    Introduce accounts helper #4
    * Simplify retrieval of user accounts from environment

--- internal/app/router/router.go
+++ internal/app/router/router.go
@@ -3,12 +3,13 @@ package router
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/neverhaveiever-io/api/internal/app/auth"
 	"github.com/neverhaveiever-io/api/internal/app/middleware/error"
 	"github.com/neverhaveiever-io/api/internal/app/middleware/prometheus"
 	v1 "github.com/neverhaveiever-io/api/internal/app/router/v1"
 )
 
-func Init(accounts gin.Accounts) {
+func Init() {
 
 	router := gin.Default()
 
@@ -16,11 +17,11 @@ func Init(accounts gin.Accounts) {
 	router.Use(error.Error())
 
 	// initialize prometheus metrics
-	prometheus.UseWithAuth(router, accounts)
+	prometheus.UseWithAuth(router, auth.Accounts([]string{"admin", "metrics"}))
 
 	router.GET("/v1/statements/:id", v1.GetStatement)
 
-	authorized := router.Group("/v1", gin.BasicAuth(accounts))
+	authorized := router.Group("/v1", gin.BasicAuth(auth.Accounts([]string{"admin"})))
 	{
 		authorized.POST("/statements", v1.AddStatement)
 		authorized.PUT("/statements/:id", v1.EditStatement)
