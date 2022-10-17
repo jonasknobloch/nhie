ffb2277ea5dabf477daaa49391450f2b77c755dc
Jonas Knobloch <jonas.knobloch@t-online.de>
2022-10-17 15:53:25 +0200

    Set GORM log level to error

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,12 +3,14 @@ package database
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var C *gorm.DB
 
 func Init(dsn string) error {
 	client, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger:      logger.Default.LogMode(logger.Error),
 		PrepareStmt: true,
 	})
 
