0a39c5261fab91d2a69020e90c9ca66a330097ea
Jonas Knobloch <jonas.knobloch@t-online.de>
2022-12-12 11:29:50 +0100

    Ident with two spaces

--- web/app.js
+++ web/app.js
@@ -9,103 +9,103 @@ import {NHIELinearProgress} from "./src/linear-progress";
 import {NHIEStatement} from './src/statement';
 
 document.addEventListener('DOMContentLoaded', () => {
-    limitSearchParams('language');
+  limitSearchParams('language');
 
-    const statement = new NHIEStatement();
+  const statement = new NHIEStatement();
 
-    const categories = [].slice.call(document.querySelectorAll('.nhie-category__input')).map((e) => new NHIECategory(e));
+  const categories = [].slice.call(document.querySelectorAll('.nhie-category__input')).map((e) => new NHIECategory(e));
 
-    const categorySelection = new NHIECategorySelection();
-    const languageSelection = new NHIELanguageSelection();
+  const categorySelection = new NHIECategorySelection();
+  const languageSelection = new NHIELanguageSelection();
 
-    const linearProgress = new NHIELinearProgress();
-    const colorScheme = new NHIEColorScheme();
+  const linearProgress = new NHIELinearProgress();
+  const colorScheme = new NHIEColorScheme();
 
-    const client = new NHIEClient();
+  const client = new NHIEClient();
 
-    categorySelection.registerCategories(...categories);
+  categorySelection.registerCategories(...categories);
 
-    client.registerFeature('statement_id', () => statement.ID);
-    client.registerFeature('category', () => categorySelection.activeCategories.map((c) => c.key));
-    client.registerFeature('language', () => languageSelection.activeLanguage);
-    client.registerFeature('invert_color_scheme', () => colorScheme.invertColorScheme ? 'true' : 'false');
+  client.registerFeature('statement_id', () => statement.ID);
+  client.registerFeature('category', () => categorySelection.activeCategories.map((c) => c.key));
+  client.registerFeature('language', () => languageSelection.activeLanguage);
+  client.registerFeature('invert_color_scheme', () => colorScheme.invertColorScheme ? 'true' : 'false');
 
-    document.addEventListener('keyup', (keyboardEvent) => {
-        if (keyboardEvent.code === 'Space') {
-            refreshStatement();
-        }
-    });
-
-    document.querySelector('main').addEventListener('click', () => {
-        refreshStatement();
-    });
-
-    document.addEventListener('language-changed', () => {
-        let url = new URL(window.location.href);
-        client.encodeFeatures(url, client.limitFeatures('language', 'category', 'invert_color_scheme'));
-        window.location.assign(url);
-    });
-
-    let refreshing = false;
-
-    function refreshStatement() {
-        if (!refreshing) {
-            refreshing = true;
-            linearProgress.on();
-
-            client.fetchStatement()
-                .then((response) => {
-                    return response.json()
-                })
-                .then((result) => {
-                    replaceStatementID(result.ID);
-                    return result;
-                })
-                .then((result) => {
-                    statement.ID = result.ID;
-                    statement.statement = result.statement;
-                })
-                .catch((error) => {
-                    console.log(error);
-                    statement.statement = 'Something went wrong.';
-                })
-                .finally(() => {
-                    refreshing = false;
-                    linearProgress.off();
-                });
-        }
+  document.addEventListener('keyup', (keyboardEvent) => {
+    if (keyboardEvent.code === 'Space') {
+      refreshStatement();
     }
+  });
+
+  document.querySelector('main').addEventListener('click', () => {
+    refreshStatement();
+  });
+
+  document.addEventListener('language-changed', () => {
+    let url = new URL(window.location.href);
+    client.encodeFeatures(url, client.limitFeatures('language', 'category', 'invert_color_scheme'));
+    window.location.assign(url);
+  });
+
+  let refreshing = false;
+
+  function refreshStatement() {
+    if (!refreshing) {
+      refreshing = true;
+      linearProgress.on();
+
+      client.fetchStatement()
+        .then((response) => {
+          return response.json()
+        })
+        .then((result) => {
+          replaceStatementID(result.ID);
+          return result;
+        })
+        .then((result) => {
+          statement.ID = result.ID;
+          statement.statement = result.statement;
+        })
+        .catch((error) => {
+          console.log(error);
+          statement.statement = 'Something went wrong.';
+        })
+        .finally(() => {
+          refreshing = false;
+          linearProgress.off();
+        });
+    }
+  }
 });
 
 function limitSearchParams() {
-    let url = new URL(window.location.href);
-    let keys = Array.from(arguments);
+  let url = new URL(window.location.href);
+  let keys = Array.from(arguments);
 
-    let removals = Array();
+  let removals = Array();
 
-    url.searchParams.forEach((value, key, ) => {
-        if (keys.includes(key)) {
-             return;
-        }
+  url.searchParams.forEach((value, key, ) => {
+    if (keys.includes(key)) {
+      return;
+    }
 
-        if (removals.includes(key)) {
-            return;
-        }
+    if (removals.includes(key)) {
+      return;
+    }
 
-        removals.push(key);
-    });
+    removals.push(key);
+  });
 
-    removals.forEach((key) => {url.searchParams.delete(key)})
+  removals.forEach((key) => {url.searchParams.delete(key)})
 
-    window.history.replaceState({}, document.title, url);
+  window.history.replaceState({}, document.title, url);
 }
 
 function replaceStatementID(ID) {
-    let url = new URL(window.location.href);
-    let path = url.pathname.split('/');
+  let url = new URL(window.location.href);
+  let path = url.pathname.split('/');
 
-    path[path.length - 1] = ID;
-    url.pathname = path.join('/');
+  path[path.length - 1] = ID;
+  url.pathname = path.join('/');
 
-    window.history.replaceState({}, document.title, url);
+  window.history.replaceState({}, document.title, url);
 }
--- web/src/linear-progress/index.js
+++ web/src/linear-progress/index.js
@@ -1,15 +1,15 @@
 import './style.css';
 
 export class NHIELinearProgress {
-    constructor() {
-        this.rootElement = document.querySelector('.nhie-linear-progress');
-    }
+  constructor() {
+    this.rootElement = document.querySelector('.nhie-linear-progress');
+  }
 
-    on() {
-        this.rootElement.classList.remove( 'nhie-linear-progress--disabled');
-    }
+  on() {
+    this.rootElement.classList.remove('nhie-linear-progress--disabled');
+  }
 
-    off() {
-        this.rootElement.classList.add('nhie-linear-progress--disabled');
-    }
+  off() {
+    this.rootElement.classList.add('nhie-linear-progress--disabled');
+  }
 }
