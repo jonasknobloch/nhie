77d6e661733b4bf4caf021c7ab8dc76af9053b80
Jonas Knobloch <jonas.knobloch@t-online.de>
2023-01-19 17:41:09 +0100

    Add table constraints

--- scheme.sql
+++ scheme.sql
@@ -29,3 +29,15 @@ CREATE TABLE translations
     created_at   timestamp with time zone NOT NULL,
     updated_at   timestamp with time zone NOT NULL
 );
+
+ALTER TABLE ONLY statements
+    ADD CONSTRAINT statements_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY statements
+    ADD CONSTRAINT statements_statement_key UNIQUE (statement);
+
+ALTER TABLE ONLY translations
+    ADD CONSTRAINT translations_pkey PRIMARY KEY (statement_id, language);
+
+ALTER TABLE ONLY translations
+    ADD CONSTRAINT statement_id_fkey FOREIGN KEY (statement_id) REFERENCES public.statements (id) ON UPDATE CASCADE ON DELETE CASCADE;
